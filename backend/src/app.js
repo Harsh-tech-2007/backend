a6d63e392ff06ca4987348a85854f20cd8b58c77
Harsh-tech-2007 <harshop885@gmail.com>
2026-09-19 16:54:32 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: "https://skill-sync-8nap.onrender.com",
     credentials: true,
 }
 
