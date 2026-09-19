7f76b4ec06ffe2da62e7f3b6bc3d38336fc6c436
Harsh-tech-2007 <harshop885@gmail.com>
2026-09-19 17:17:59 +0530

    Update auth_con.js

--- backend/src/controller/auth_con.js
+++ backend/src/controller/auth_con.js
@@ -138,7 +138,12 @@ async function logoutUserController(req, res) {
             await tokenBlacklistModel.create({ token })
         }
 
-        res.clearCookie("token")
+        
+        res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+})
 
         res.status(200).json({
             message: "User logged out successfully"
