2d3041ac6d238f1f229052611c8513cd74fa1da9
Harsh-tech-2007 <harshop885@gmail.com>
2026-09-19 16:37:06 +0530

    Change API base URL to production server

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:4000", 
+    baseURL: "https://skill-sync-backend-asvm.onrender.com", 
     withCredentials: true
 })
 
