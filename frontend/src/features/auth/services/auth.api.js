238a8570e93b9530ed523d53d53198d47694fc70
Harsh-tech-2007 <harshop885@gmail.com>
2026-09-20 13:58:06 +0530

    Change API base URL for authentication service

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "https://skill-sync-backend-asvm.onrender.com", 
+    baseURL:"https://api.mathiverse.in", 
     withCredentials: true
 })
 
