23065762b810f2bf41e15e0fdf5ea62dc46922b5
Harsh-tech-2007 <harshop885@gmail.com>
2026-09-20 14:23:57 +0530

    Update SameSite attribute for cookies in auth controller
    Changed SameSite cookie attribute from 'none' to 'lax' for improved CSRF protection.

--- backend/src/controller/auth_con.js
+++ backend/src/controller/auth_con.js
@@ -46,7 +46,7 @@ async function registerUserController(req, res) {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000,
             secure: true, 
-            sameSite: "none"                             // protect against CSRF
+            sameSite: "lax"                             // protect against CSRF
         })
 
         res.status(201).json({
@@ -106,7 +106,7 @@ async function loginUserController(req, res) {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000,
             secure: true,
-            sameSite: "none"                            
+            sameSite: "lax"                            
         })
 
         res.status(200).json({
